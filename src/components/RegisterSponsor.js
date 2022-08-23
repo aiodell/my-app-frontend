b06985ff9be981a530e5c0831672258a8d297528
Kelly Duran <kaduran16@gmail.com>
2022-08-23 15:02:37 -0400

    register sponsor form

--- src/components/RegisterSponsor.js
+++ src/components/RegisterSponsor.js
@@ -1,8 +1,6 @@
-/* eslint-disable no-undef */
 import React from "react";
 import Form from 'react-bootstrap/Form';
 import { useState } from "react";
-
 
 function RegisterSponsor(){
     const [inputName, setInputName] = useState('')
@@ -16,35 +14,27 @@ function RegisterSponsor(){
         }
     }
 
-    fetch( 'http://localhost:9292/addsponsor', {
+    fetch( 'http://localhost:9292/addsponsors', {
         method: "POST",
         headers: {
           'Accept': 'application.json',
           'Content-Type': 'application/json'
         },
-        // eslint-disable-next-line no-undef
         body: JSON.stringify(newSponsor)
       })
       .then(res => res.json())
       .then((data) => console.log(data))
   
-      // eslint-disable-next-line no-undef
       submitFunction(newSponsor)
-      // eslint-disable-next-line no-undef
       e.target.reset()
-    }
+    
 
-
-
- 
-
-
+    return(
         <div>
             <Form>
                 <h3>Sponsor Sign Up</h3>
                 <Form.Group className="input-text" controlID="name" >
                     <Form.Label>Name</Form.Label>
-                    // eslint-disable-next-line no-undef, no-undef, no-undef, no-undef
                     <Form.Control type="text" placeholder="Enter first and last name" onChange={(e) => setInputName(e.target.value)}/>
                 </Form.Group>
                 <br />
@@ -61,8 +51,7 @@ function RegisterSponsor(){
                 />
             </Form>
         </div>
-    
-
+    )
+}
 
 export default RegisterSponsor
-

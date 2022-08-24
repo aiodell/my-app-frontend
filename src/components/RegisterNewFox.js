708bf388ceeb85495a62f99af72fd24a32b4fbe1
Andrianna <andrianna.odell@gmail.com>
2022-08-24 07:42:00 -0400

    cleaned up register fox code

--- src/components/RegisterNewFox.js
+++ src/components/RegisterNewFox.js
@@ -1,35 +1,28 @@
 import React, { useState } from "react";
 import Form from 'react-bootstrap/Form';
 import Container from 'react-bootstrap/Container'
+import Button from 'react-bootstrap/Button'
 
 function RegisterNewFox({ addFox }) {
-    const [ name, setName ] = useState('')
-    const [ age, setAge ] = useState(0)
-    const [ personality, setPersonality ] = useState('')
-    const [ favoriteToy, setFavoriteToy ] = useState('')
-    const [ image, setImage ] = useState('')
-
-    // submit the fox information
-    function handleSubmit(e) {
+    const [formData, setFormData] = useState({ })
+    
+    const handleSubmit = (e) => {
         e.preventDefault();
-        const newFox = {
-            name: name,
-            age: age,
-            personality: personality,
-            favorite_toy: favoriteToy,
-            image_url: image
-        }
 
-        fetch('http://localhost:9292/foxes', {
-            method : "POST",
-            headers : { "Content-Type" : "application/json"},
-            body : JSON.stringify(newFox)
+          fetch("http://localhost:9292/foxes",{
+          method : "POST",
+          headers : { "Content-Type" : "application/json"},
+          body : JSON.stringify(formData)
         })
-
-        .then(res => res.json())
-        .then((foxData) => {addFox(foxData)})
-        e.target.reset()
+        .then(resp => resp.json())
+        .then((foxData) => {
+          addFox(foxData);
+          setFormData({ });
+          e.target.reset();
+        })
     }
+
+    function handleChange(e){ setFormData({...formData, [e.target.id] : e.target.value }) }
 
     return(
         <Container>
@@ -37,43 +30,36 @@ function RegisterNewFox({ addFox }) {
             <Form onSubmit = { handleSubmit }>
 
                 {/* Enter fox name */}
-                <Form.Group className="mb-3" controlId="name" onChange= {(e) => setName(e.target.value)}>
+                <Form.Group className="mb-3" controlId="name" onChange= { handleChange }>
                     <Form.Label>Name: </Form.Label>
                     <Form.Control type="text" placeholder= "Fox name"/>
                 </Form.Group>
 
                 {/* Enter fox age */}
-                <Form.Group className="mb-3" controlId="age" onChange= {(e) => setAge(e.target.value)}>
+                <Form.Group className="mb-3" controlId="age" onChange= { handleChange }>
                     <Form.Label>Age: </Form.Label>
                     <Form.Control type="number" placeholder= "Fox age"/>
                 </Form.Group>
                 
                 {/* Enter fox toy */}
-                <Form.Group className="mb-3" controlId="age" onChange= {(e) => setFavoriteToy(e.target.value)}>
+                <Form.Group className="mb-3" controlId="favorite_toy" onChange= { handleChange }>
                     <Form.Label>Favorite Toy: </Form.Label>
                     <Form.Control type="text" placeholder= "Favorite toy"/>
                 </Form.Group>
 
-                {/* Enter persoanality */}
-                <Form.Group className="mb-3" controlId="personality" onChange= {(e) => setPersonality(e.target.value)}>
+                {/* Enter personality */}
+                <Form.Group className="mb-3" controlId="personality" onChange= { handleChange }>
                     <Form.Label>Personality: </Form.Label>
                     <Form.Control type="text" placeholder= "Enter personality"/>
                 </Form.Group>
 
                 {/* Enter fox personality */}
-                <Form.Group className="fox-image" controlid="image_url" onChange= {(e) => setImage(e.target.value)}>
+                <Form.Group className="fox-image" controlid="image_url" onChange= { handleChange }>
                     <Form.Label>Image: </Form.Label>
                     <Form.Control type="text" placeholder= "Show the floof"/>
                 </Form.Group>
-
                 <br />
-                <input
-                    type="submit"
-                    name="submit"
-                    value="Register"
-                    className="submit"
-                />
-
+                <Button type ="submit" variant="success">Register Fox</Button>
             </Form>
         </Container>
     )
